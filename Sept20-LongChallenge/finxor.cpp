88ed2b6deb021eba26539bc428f7d97ae37ac6f5
Shubham <shubhamyadav2002@gmail.com>
2020-09-06 00:29:07 +0530

    Added new files

--- Sept20-LongChallenge/finxor.cpp
+++ Sept20-LongChallenge/finxor.cpp
@@ -3,34 +3,48 @@
 
 using namespace std;
 
-void subMain()	{
-	int n;
+int subMain()	{
+	long long n, answer = 0;
 	cin >> n;
-	int a[n];
-	int sum = 0, x;
-	for(int i=0;i<n;i++)	{
+	vector<long long>xorsum;
+	long long x;
+	for(long long i=1;i<=20;i++)	{
+		cout << 1 << " " << (1ul<<i) << endl;
+		cout.flush();
 		cin >> x;
-		a[i] = x;
-		sum = (sum^x);
-	}	
-	cout << sum << endl;
-	int k;
-	cin >> k;
-	int sum1 = 0;
-	for(int i=0;i<n;i++)	{
-		sum1+=(a[i]^k);
+		xorsum.push_back(x);
 	}
-	cout << sum1 << endl;
+	reverse(xorsum.begin(), xorsum.end());
+	long long sum = xorsum[0]-n*(1ul<<20);
+	for(long long i=1;i<xorsum.size();i++)	{
+		if(xorsum[i] >= sum)	{
+			xorsum[i] = ((n-(xorsum[i]-sum)/(1ul<<(xorsum.size()-i)))/2);
+		}
+		else
+			xorsum[i] = (n+(sum-xorsum[i])/(1ul<<(xorsum.size()-i)))/2;
+	}
+	for(int i=1;i<xorsum.size();i++)
+		if(xorsum[i]%2 != 0)
+			answer+=1ul<<(xorsum.size()-i);
+	if(sum%2!=0)
+		answer++;
+	cout << 2 << " " << answer << endl;
+	cout.flush();
+	int result;
+	cin >> result;
+	return result;
 }
 
 int main()	{
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 	int t = 1;
-	//cin >> t;
+	cin >> t;
 	while(t--)	{
-		subMain();
-		cout << "\n";
+		if(!subMain())
+			break;
+		cout << endl;
+		cout.flush();
 	}	
 	return 0;
 }
